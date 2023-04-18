c552f3157fb4d20d1fb0d55c2878ab7584f2cca9
Andrew Lee <candrewlee14@gmail.com>
2023-04-18 10:25:16 -0600

    add embedded tutorial video

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -3,3 +3,4 @@
     <h1 class="text-center">User Guide</h1>
   </div>
 </div>
+<iframe class="pt-4" width="560" height="315" src="https://www.youtube.com/embed/6Ghccx52264" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
